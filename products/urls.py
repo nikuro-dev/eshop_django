2af3f6fd4215102c8ee18f215ba4d9d8b587fe74
amirhossein <nikuro-dev>
2023-02-06 01:37:40 +0330

    some refactoring

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from products.views import product_list, product_detail
+
+#urls here
+
 urlpatterns = [
-    path('list', views.product_list),
-    path('<slug:slug>', views.product_detail, name='product_detail'),
+    path('list', product_list),
+    path('<slug:slug>', product_detail, name='product_detail'),
 ]
