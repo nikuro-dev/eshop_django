cf04ee5a4fbea055d22e8edbe7e0e3ce357963f7
amirhossein <nikuro-dev>
2023-02-06 13:11:29 +0330

    add views for contact page and components

--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index_page(request):
     return render(request, 'home_module/index_page.html',{})
+
+def contact_page(request):
+    return render(request, 'home_module/contact_page.html')
+
+def site_header_component(request):
+    return render(request, 'shared/site_header_component.html')
+
+def site_footer_component(request):
+    return render(request, 'shared/site_footer_component.html')
