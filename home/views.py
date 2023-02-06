806e909379f4729253e2142bd1eac2b7976671dc
amirhossein <nikuro-dev>
2023-02-06 12:32:14 +0330

    connect view funvtion to teplate

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_page(request):
-    return render('', 'home_module/index_page.html',{})
+    return render(request, 'home_module/index_page.html',{})
