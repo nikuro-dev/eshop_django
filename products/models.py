332a58445045f55042193a1c0226729f319a8758
amirhossein <nikuro-dev>
2023-02-07 02:46:34 +0330

    add brand model

--- products/models.py
+++ products/models.py
@@ -17,11 +17,23 @@ class ProductCategory(models.Model):
     class Meta:
         verbose_name = 'دسته بندی'
         verbose_name_plural = 'دسته بندی ها'
-    
 
+class ProductBrand(models.Model):
+    title = models.CharField(max_length=100, verbose_name='نام برند', db_index=True)
+    is_active = models.BooleanField()
+    
+    class Meta:
+        verbose_name = 'برند'
+        verbose_name_plural = 'تمامی برندها'   
+
+    def __str__(self) -> str:
+        return self.title
+    
+    
 class Product(models.Model):
     title = models.CharField(max_length=300, verbose_name='نام محصول')
     slug = models.SlugField(default="", null=False, db_index=True,blank=True, unique=True,verbose_name='عنوان در url')
+    brand = models.ForeignKey(ProductBrand, on_delete= models.CASCADE, verbose_name='برند')
     category = models.ManyToManyField(ProductCategory, related_name='product_categories', verbose_name='دسته بندی ها')
     price = models.IntegerField(verbose_name='قیمت')
     short_description = models.CharField(max_length=360, null=True, db_index=True, verbose_name='توضیحات کوتاه')
