fa4fc4599e81a8e4bc294e4bbe95999d31df1499
amirhossein <nikuro-dev>
2023-02-06 13:11:53 +0330

    mini changes

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from home.views import index_page
+from home.views import index_page, contact_page
 
 #urls here
 
 urlpatterns = [
-    path('', index_page, name='home-page')
+    path('', index_page, name='home-page'),
+    path('contact-us', contact_page, name='contact-us')
 ]
